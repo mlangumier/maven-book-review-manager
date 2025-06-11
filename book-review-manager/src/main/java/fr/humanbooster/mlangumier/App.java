bd2d962273ce8ae9c473431b3549ac6e4e4b7280
Mathieu Langumier <mathieu.langumier@gmail.com>
2025-06-11 16:58:50 +0200

    Feat: working on methods

--- book-review-manager/src/main/java/fr/humanbooster/mlangumier/App.java
+++ book-review-manager/src/main/java/fr/humanbooster/mlangumier/App.java
@@ -1,7 +1,13 @@
 package fr.humanbooster.mlangumier;
 
+import fr.humanbooster.mlangumier.model.Book;
+import fr.humanbooster.mlangumier.model.Review;
 import fr.humanbooster.mlangumier.service.BookService;
 import fr.humanbooster.mlangumier.service.ReviewService;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Hello world!
@@ -13,7 +19,20 @@ public class App {
         BookService bookService = new BookService();
         ReviewService reviewService = new ReviewService();
 
-        System.out.println("----- BOOKS\n" + bookService.getBooks());
-        System.out.println("----- REVIEWS\n" + reviewService.getReviews());
+        // List books (sort by average rating, desc)
+        // 1. Review - Get average-rating for each bookId
+        // 2. Review - map & display book - rating
+
+        // List reviews from a specific author (sort by date, desc)
+        List<Review> reviewFromAuthor = reviewService.getReviews().stream().filter(review -> review.getUsername().equals("Alice")).toList();
+        System.out.println("\n>> Reviews by Alice: \n" +  reviewFromAuthor);
+
+        // For each genre, get the book with the highest rating (sort by rating, desc)
+
+        // List reviews from books released before a given date
+        // Book - Get books released before [date]
+        // Reviews - Get reviews for all books (or IDs?) from list
+
+        // Display the number of books and the average rating of each genre
     }
 }
